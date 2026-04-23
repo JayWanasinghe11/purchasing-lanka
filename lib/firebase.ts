7a48a4a0a903cb7b12c1b9fa7180738d01803e0d
Jayamantha_Wanasinghe <jayamanthawanasinghe11@gmail.com>
2026-04-24 02:09:02 +0530

    Securely configured Firebase using env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,18 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQbgVOlpX48HysT1gBh0-Fli_SBZIZWlc",
-  authDomain: "purchasing-lanka.firebaseapp.com",
-  projectId: "purchasing-lanka",
-  storageBucket: "purchasing-lanka.firebasestorage.app",
-  messagingSenderId: "1889322398",
-  appId: "1:1889322398:web:e251bc08a13810565e428f",
-  measurementId: "G-9GBBLJ1WKS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-
+// Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
